cf7b1b4017971a1ec6d7f8782b5dfa89bab9f7d4
Kevin Hou <kevin.ch.hou@gmail.com>
2017-10-21 12:52:01 -0400

    Updated user schema in model

---
+++ docs/user.md
@@ -0,0 +1,5 @@
+link Name, email, school level/subjects taught, interests, description that teachers can edit with info about them and what they're looking for 
+link twitter accounts (perhaps embed a view of their running feed; something cool could be to only display tweets with a certain hashtag, although this feature would be long-term)
+Display facebook profile, vox account name, etc.
+Give them the ability to upload links or mini "posts" (picture + couple of lines of text?) of their past projects or lesson plans on their page. These could be hosted on the website or, probably better, just links to posts they have elsewhere
+Home page (right after log in ) could be a Tigerbook page but for "IgniteSTEM", featuring our social media channels and description. 
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,17 +1,39 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    organization: DataTypes.STRING
-  }, {
+    var User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+        },
+        password: {
+            type: DataTypes.STRING,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+        },
+        school: {
+            type: DataTypes.STRING,
+        },
+        subjects: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+        },
+        interests: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+        },
+        bio: {
+            type: DataTypes.STRING,
+        },
+        twitter_username: {
+            type: DataTypes.STRING,
+        },
+    }, {
     classMethods: {
-      associate: function(models) {
+        associate: function(models) {
         // associations can be defined here
       }
     }
-  });
-  return User;
+    });
+    return User;
 };
