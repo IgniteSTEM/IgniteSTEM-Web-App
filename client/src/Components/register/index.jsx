628359f5eabbcce0db572ac289cedef29ef4bf70
Kevin Hou <kevin.ch.hou@gmail.com>
2017-12-18 13:00:57 -0800

    Added register UI elements

--- client/src/Components/register/index.jsx
+++ client/src/Components/register/index.jsx
@@ -10,7 +10,12 @@ class Register extends Component {
         this.state ={
             data: {
                 username: '',
-                password: '',
+                email: '',
+                occupation: '',
+                school: '',
+                schoolType: '',
+                password: ''
+                confirmPassword: '',
             }
         };
     }
@@ -18,7 +23,66 @@ class Register extends Component {
     render() {
         return (
             <div className="register">
-                Register
+                <h3>Register For IgniteSTEM</h3>
+                <form onSubmit={(event) => this.login(event)}>
+                    <p>Username</p>
+                    <input
+                        className="input-text"
+                        type="text"
+                        id="username"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Email</p>
+                    <input
+                        className="input-text"
+                        type="text"
+                        id="email"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Occupation</p>
+                    <input
+                        className="input-text"
+                        type="text"
+                        id="occupation"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Which school do you work at?</p>
+                    <input
+                        className="input-text"
+                        type="text"
+                        id="school"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Do you teach/lead a primary, elementary, middle, or high school?</p>
+                    <input
+                        className="input-text"
+                        type="text"
+                        id="schoolType"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Password</p>
+                    <input
+                        className="input-text"
+                        type="password" id="password"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <p>Confirm Password</p>
+                    <input
+                        className="input-text"
+                        type="password" id="confirmPassword"
+                        onChange={(event) => this.editField(event)}
+                    />
+
+                    <br />
+
+                    <input className="input-button" type="submit" value="REGISTER" />
+                </form>
             </div>
         );
     }
